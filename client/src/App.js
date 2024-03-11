39a509813fcfc33eaf8af0ec930474308004bee2
Gurukiran2034 <guru@infodrive-solutions.com>
2024-03-11 13:22:53 +0530

    new

--- client/src/App.js
+++ client/src/App.js
@@ -5,15 +5,22 @@ import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import Header from "./component/Header";
 import Footer from "./component/Footer";
 import Home from "./pages/Home";
+import Exploretypes from "./component/Exploretypes";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+
+
 
 const Layout = () => {
   return (
-    <>
-      <Header />
+    // <>
+    //   <Header />
 
-      <Outlet />
-      <Footer />
-    </>
+    //   <Outlet />
+    //   <Footer />
+    // </>
+    <Exploretypes></Exploretypes>
   );
 };
 
